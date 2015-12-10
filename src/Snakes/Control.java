888401518edb4697c4974e8cf4e7729bfd3c1f7d
andytang108 <tang147@purdue.edu>
2015-12-09 23:18:07 -0500

    2222

--- src/Snakes/Control.java
+++ src/Snakes/Control.java
@@ -14,53 +14,81 @@ import java.awt.event.KeyListener;
 
 
 public class Control implements KeyListener{
-    Model model;
+    Model model = null;
+    ServerModel smodel = null;
 
     public Control(Model model){
         this.model = model;
     }
     public Control(ServerModel model){
-        this.model = model;
+        this.smodel = model;
     }
 
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
-        if (model.running){
-            switch (keyCode) {
-                case KeyEvent.VK_UP:
-                    model.changeDirection(Model.UP);
-                    break;
-                case KeyEvent.VK_DOWN:
-                    model.changeDirection(Model.DOWN);
-                    break;
-                case KeyEvent.VK_LEFT:
-                    model.changeDirection(Model.LEFT);
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    model.changeDirection(Model.RIGHT);
-                    break;
-                case KeyEvent.VK_ADD:
-                case KeyEvent.VK_PAGE_UP:
-                    model.speedUp();
-                    break;
-                case KeyEvent.VK_SUBTRACT:
-                case KeyEvent.VK_PAGE_DOWN:
-                    model.speedDown();
-                    break;
-                case KeyEvent.VK_SPACE:
-                case KeyEvent.VK_P:
-                    model.changePauseState();
-                    break;
-                default:
+        if(model != null){
+            if (model.running){
+                switch (keyCode) {
+                    case KeyEvent.VK_UP:
+                        model.changeDirection(Model.UP);
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        model.changeDirection(Model.DOWN);
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        model.changeDirection(Model.LEFT);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        model.changeDirection(Model.RIGHT);
+                        break;
+                    case KeyEvent.VK_ADD:
+                    case KeyEvent.VK_PAGE_UP:
+                        model.speedUp();
+                        break;
+                    case KeyEvent.VK_SUBTRACT:
+                    case KeyEvent.VK_PAGE_DOWN:
+                        model.speedDown();
+                        break;
+                    case KeyEvent.VK_SPACE:
+                    case KeyEvent.VK_P:
+                        model.changePauseState();
+                        break;
+                    default:
+                }
             }
         }
-
-        // 任何情况下处理的按键，按键导致重新启动游戏
-        if (keyCode == KeyEvent.VK_R ||
-                keyCode == KeyEvent.VK_S ||
-                keyCode == KeyEvent.VK_ENTER) {
-            model.reset();
+        if(smodel != null){
+            if (smodel.running){
+                switch (keyCode) {
+                    case KeyEvent.VK_UP:
+                        smodel.changeDirection2(Model.UP,1);
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        smodel.changeDirection2(Model.DOWN,1);
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        smodel.changeDirection2(Model.LEFT,1);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        smodel.changeDirection2(Model.RIGHT,1);
+                        break;
+                    case KeyEvent.VK_ADD:
+                    case KeyEvent.VK_PAGE_UP:
+                        smodel.speedUp();
+                        break;
+                    case KeyEvent.VK_SUBTRACT:
+                    case KeyEvent.VK_PAGE_DOWN:
+                        smodel.speedDown();
+                        break;
+                    case KeyEvent.VK_SPACE:
+                    case KeyEvent.VK_P:
+                        smodel.changePauseState();
+                        break;
+                    default:
+                }
+            }
         }
+       
     }
 
     public void keyReleased(KeyEvent e) {
